78c6e4f49ee40753c9f01f4870b1e6b2ae4c0165
Jean-Louis Dikasa <81484470+Jdikasa@users.noreply.github.com>
2021-09-19 15:20:07 +0100

    Update bindings.php

--- src/bindings.php
+++ src/bindings.php
@@ -1,34 +1,34 @@
 <?php
 
-use Marquine\Etl\Container;
+use Jdikasa\Etl\Container;
 
 $container = Container::getInstance();
 
 // Database
-$container->singleton(Marquine\Etl\Database\Manager::class);
-$container->alias(Marquine\Etl\Database\Manager::class, 'db');
+$container->singleton(Jdikasa\Etl\Database\Manager::class);
+$container->alias(Jdikasa\Etl\Database\Manager::class, 'db');
 
 // Extractors
-$container->bind('collection_extractor', Marquine\Etl\Extractors\Collection::class);
-$container->bind('csv_extractor', Marquine\Etl\Extractors\Csv::class);
-$container->bind('fixed_width_extractor', Marquine\Etl\Extractors\FixedWidth::class);
-$container->bind('json_extractor', Marquine\Etl\Extractors\Json::class);
-$container->bind('query_extractor', Marquine\Etl\Extractors\Query::class);
-$container->bind('table_extractor', Marquine\Etl\Extractors\Table::class);
-$container->bind('xml_extractor', Marquine\Etl\Extractors\Xml::class);
+$container->bind('collection_extractor', Jdikasa\Etl\Extractors\Collection::class);
+$container->bind('csv_extractor', Jdikasa\Etl\Extractors\Csv::class);
+$container->bind('fixed_width_extractor', Jdikasa\Etl\Extractors\FixedWidth::class);
+$container->bind('json_extractor', Jdikasa\Etl\Extractors\Json::class);
+$container->bind('query_extractor', Jdikasa\Etl\Extractors\Query::class);
+$container->bind('table_extractor', Jdikasa\Etl\Extractors\Table::class);
+$container->bind('xml_extractor', Jdikasa\Etl\Extractors\Xml::class);
 
 // Transformers
-$container->bind('callback_transformer', Marquine\Etl\Transformers\Callback::class);
-$container->bind('convert_case_transformer', Marquine\Etl\Transformers\ConvertCase::class);
-$container->bind('json_decode_transformer', Marquine\Etl\Transformers\JsonDecode::class);
-$container->bind('json_encode_transformer', Marquine\Etl\Transformers\JsonEncode::class);
-$container->bind('number_format_transformer', Marquine\Etl\Transformers\NumberFormat::class);
-$container->bind('pad_transformer', Marquine\Etl\Transformers\Pad::class);
-$container->bind('rename_columns_transformer', Marquine\Etl\Transformers\RenameColumns::class);
-$container->bind('replace_transformer', Marquine\Etl\Transformers\Replace::class);
-$container->bind('trim_transformer', Marquine\Etl\Transformers\Trim::class);
-$container->bind('unique_rows_transformer', Marquine\Etl\Transformers\UniqueRows::class);
+$container->bind('callback_transformer', Jdikasa\Etl\Transformers\Callback::class);
+$container->bind('convert_case_transformer', Jdikasa\Etl\Transformers\ConvertCase::class);
+$container->bind('json_decode_transformer', Jdikasa\Etl\Transformers\JsonDecode::class);
+$container->bind('json_encode_transformer', Jdikasa\Etl\Transformers\JsonEncode::class);
+$container->bind('number_format_transformer', Jdikasa\Etl\Transformers\NumberFormat::class);
+$container->bind('pad_transformer', Jdikasa\Etl\Transformers\Pad::class);
+$container->bind('rename_columns_transformer', Jdikasa\Etl\Transformers\RenameColumns::class);
+$container->bind('replace_transformer', Jdikasa\Etl\Transformers\Replace::class);
+$container->bind('trim_transformer', Jdikasa\Etl\Transformers\Trim::class);
+$container->bind('unique_rows_transformer', Jdikasa\Etl\Transformers\UniqueRows::class);
 
 // Loaders
-$container->bind('insert_loader', Marquine\Etl\Loaders\Insert::class);
-$container->bind('insert_update_loader', Marquine\Etl\Loaders\InsertUpdate::class);
+$container->bind('insert_loader', Jdikasa\Etl\Loaders\Insert::class);
+$container->bind('insert_update_loader', Jdikasa\Etl\Loaders\InsertUpdate::class);
